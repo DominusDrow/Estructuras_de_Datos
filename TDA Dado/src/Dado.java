197e5270d66b52a3acca3f24f484ed4ed5aeb0d8
Drow <Drow@localhost>
2021-01-22 12:23:57 -0600

    version 1

---
+++ TDA Dado/src/Dado.java
@@ -0,0 +1,71 @@
+import java.util.Random;
+
+/**
+ * “Dado” es un tipo de dato abstracto en java que contiene dentro de sí una variable de tipo 
+ * entero, la cual guardará el estado (1 al  6 o 0 si no se ha tirado) del dado, a este se le 
+ * asocian las operaciones básicas que permitan su funcionamiento como lanzarlo, obtener 
+ * su valor y mostrarlo, será utilizado para declarar variables y definir objetos de su tipo.
+ * @author Alfredo Omar Vasquez Lopez
+ * @version 1.0
+ * @since JDK 2.0 20/ENE/2021
+**/
+
+public class Dado {
+	
+	/**
+	 * Representa la cara del dado en la que cae 
+	 **/
+	private int caraDado;
+	
+	/**
+	 * constructor de un objeto Dado que tiene como estado
+	 * inicial "0" pues no ha sido tirado
+	 **/
+	public Dado(){
+		caraDado=0;
+	}
+	
+	/**
+	 * Método de instancia que es utilizado por un objeto de tipo “Dado” 
+	 * para simular el lanzamiento de un dado asignando al dato “caraDado” un número 
+	 * aleatorio entre [1,6], no retorna nada.
+	 **/
+	public void Lanzar() {
+		
+		Random aleatorio = new Random();
+		caraDado = aleatorio.nextInt(6)+1;
+	}
+	
+	/**
+	 * Método de instancia que es utilizado por un objeto de tipo “Dado” 
+	 * para representar su último estado, retorna un dato de tipo entero, entre [1,6]  o 0 si 
+	 * el dado aun no se ha tirado.  
+	 * @return El metodo un dato de tipo entero que representa la cara en la que el dado 
+	 * ha caído entre [1,6]
+	 * @throws ArithmeticException El metodo retorna 0 si aun no se ha tirado el dado 
+	 **/
+	public int ObtenerValor() {
+		
+		return caraDado;
+	}
+	
+	/**
+	 *  Método de instancia que es utilizado por un objeto de tipo “Dado” 
+	 *  para representar la última cara en la que el dado ha caído, retorna un dato de tipo 
+	 *  cadena que contiene un mensaje según el estado del dado.
+	 *  @return El método retorna como resultado un dato de tipo cadena cuyo mensaje 
+	 *  contiene del 1 al 6 si “caraDado!=0”
+	 *  @throws ArithmeticException  El metodo retorna un mensaje de “no se ha lanzado” 
+	 *  si “caraDado==0”
+	 **/
+	public String MostrarValor() {
+		
+		if(caraDado==0)
+			return "no se ha lanzado";
+		else
+			return "cayo en: "+caraDado+"!!!";
+	}
+	
+	
+	
+}
---
+++ TDA Dado/src/Uso_Dado.java
@@ -0,0 +1,111 @@
+import java.util.Scanner;
+
+public class Uso_Dado {
+
+	public static void main(String[] args) {
+		
+		Dado dado1 = new Dado();
+		Dado dado2 = new Dado();
+		Dado dado3 = new Dado();
+		Scanner sc= new Scanner(System.in);
+		int opc=-1,op,op2;
+		
+		while(opc!=0) {
+			op=3;
+			System.out.print("JUEGO DE MESA CON DADOS!-------------------------------------"
+				+ ":\n\nElija un modo de juego:\n1.Novato\n2.Normal\n3.Experto\n0.Salir\n\nelija: ");
+			opc=sc.nextInt();
+			
+			while(op>0) {
+				
+				if(opc==1) {
+
+					System.out.print("\nNIVEL:Novato\tDADOS:1   Oportunidades: "+op
+							+ "\nGana si consigue que caiga un 6!\n\n1.lanzar\n\nElija: ");
+					op2=sc.nextInt();
+					
+					if(op2==1)
+						dado1.Lanzar();
+					
+					System.out.print("\n\nEl dado "+dado1.MostrarValor());
+					
+					if(dado1.ObtenerValor()==6) {
+						
+						System.out.print("\nGANASTE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n");
+						op=-1;
+					}
+					else if(op>1)
+						System.out.print("\nSigue intentando:(!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n");
+					
+					
+				}
+				
+				else if(opc==2) {
+
+					System.out.print("\nNIVEL:Normal\tDADOS:2   Oportunidades: "+op
+							+ "\nGana si consigue que caiga entre los dos un 7!\n\n1.lanzar\n\nElija: ");
+					op2=sc.nextInt();
+					
+					if(op2==1) {
+						dado1.Lanzar();
+						dado2.Lanzar();
+					}
+						
+					
+					System.out.print("\n\nEl dado 1 "+dado1.MostrarValor());
+					System.out.print("\nEl dado 2 "+dado2.MostrarValor());
+					
+					if(dado1.ObtenerValor()+dado2.ObtenerValor()==7) {
+						
+						System.out.print("\nGANASTE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n");
+						op=-1;
+					}
+					else if(op>1)
+						System.out.print("\nSigue intentando:(!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n");
+					
+					
+				}
+				
+				else if(opc==3) {
+
+					System.out.print("\nNIVEL:Experto\tDADOS:3   Oportunidades: "+op
+						+ "\nGana si consigue que caiga entre los tres un 15!"
+						+ "\n\n1.lanzar\nElija: ");
+					op2=sc.nextInt();
+					
+					if(op2==1) {
+						dado1.Lanzar();
+						dado2.Lanzar();
+						dado3.Lanzar();
+					}
+						
+					
+					System.out.print("\n\nEl dado 1 "+dado1.MostrarValor());
+					System.out.print("\nEl dado 2 "+dado2.MostrarValor());
+					System.out.print("\nEl dado 3 "+dado3.MostrarValor());
+					
+					if(dado1.ObtenerValor()+dado2.ObtenerValor()+dado3.ObtenerValor()==15) {
+						
+						System.out.print("\nGANASTE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n");
+						op=-1;
+					}
+					else if(op>1)
+						System.out.print("\nSigue intentando:(!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n");
+					
+					
+				}
+				
+				if(op==1)
+					System.out.print("\nPERDISTE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n");
+				
+				op--;
+			}
+			
+		}
+		
+		System.out.print("\n\n\nJuego terminado.");
+		
+		
+	}
+
+}
